30a88802d54be123e191ac8c6e8f99b54496a1ae
SraBanach <kenya.banach94@gmail.com>
2025-02-05 11:46:33 -0300

    include header

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plataforma de agendamento</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<!-- comentario aqui tem algo a mais -->
+<body>
+    <header>
+        <nav id="topo">
+            <a href="#" class="logo">
+                <h1>Banach</h1>
+            </a>
+            <ul class="search">
+                <li> <i class="bi bi-search"></i></li>
+            </ul>
+            <ul class="login">
+                <li><a href="#">Profissional</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+        <nav>
+
+            <ul class="menu">
+                <li><a href= "#">Sobrancelhas</a> </li>
+                <li><a href= "#">Unhas</a> </li>
+                <li><a href= "#">Cilios</a> </li>
+                <li><a href= "#">Maquiagem</a> </li>
+            </ul>
+        </nav>
+    </header>
